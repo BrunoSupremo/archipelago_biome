6c166801782c1efddd02de4b9abd92fefd5df214
Bruno <bruno_mussoi@hotmail.com>
2017-08-27 13:41:50 -0300

    Added octopus encounters
    Changed starfish models
    Removed unused material/color map from cocobomb
    Crabs are not buyable anymore, fish only at tier 2
    Removed blender and extra files from octopus animations
    Updated biome thumbnail

--- data/rigs/entities/monster/octopus/spawn_octopus.lua
+++
@@ -1,3 +0,0 @@
-	local location = radiant.entities.get_world_grid_location(e)
-	local cocoling = radiant.entities.create_entity('archipelago_biome:monsters:octopus', {owner = "forest"})
-	radiant.terrain.place_entity_at_exact_location(cocoling, location)

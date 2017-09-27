257ceb51e1f261c923ca6a7c2cbbf6f766444ebd
Bruno <bruno_mussoi@hotmail.com>
2017-09-27 01:25:44 -0300

    Removed Frostfeast compatibility
    There is no point in it, people would play in temperate for that
    content, not this biome.
    Fixed incorrect crab trap description saying it could break

--- archipelago_biome_client.lua
+++ archipelago_biome_client.lua
@@ -2,15 +2,17 @@ archipelago_biome_client = {
 constants = require 'constants'
 }
 local player_service_trace = nil
+-- local log = radiant.log.create_logger('not_loading')
 
 local function check_override_ui(players, player_id)
 	if not player_id then
-		player_id = 'player_1'
+		player_id = _radiant.client.get_player_id()
 	end
 
 	local client_player = players[player_id]
 	if client_player then
 		if client_player.kingdom == "rayyas_children:kingdoms:rayyas_children" then
+			-- log:error("_radiant.res.apply_manifest")
 			_radiant.res.apply_manifest("/archipelago_biome/rayyas_children_selected/manifest.json")
 		end
 	end
@@ -21,7 +23,7 @@ local function trace_player_service()
 	:done(function(r)
 		local player_service = r.result
 		check_override_ui(player_service:get_data().players)
-		player_service_trace = player_service:trace('rayyas children ui change')
+		player_service_trace = player_service:trace('using rayyas children')
 		:on_changed(function(o)
 			check_override_ui(player_service:get_data().players)
 			end)
--- archipelago_biome_server.lua
+++ archipelago_biome_server.lua
@@ -3,21 +3,21 @@ local log = radiant.log.create_logger('version')
 log:error("Archipelago Biome mod for alpha 22.5")
 
 function archipelago_biome:_on_required_loaded()
-   local custom_world_generation_service = require('services.server.world_generation.custom_world_generation_service')
-   local world_generation_service = radiant.mods.require('stonehearth.services.server.world_generation.world_generation_service')
-   radiant.mixin(world_generation_service, custom_world_generation_service)
+	local custom_world_generation_service = require('services.server.world_generation.custom_world_generation_service')
+	local world_generation_service = radiant.mods.require('stonehearth.services.server.world_generation.world_generation_service')
+	radiant.mixin(world_generation_service, custom_world_generation_service)
 
-   local custom_landscaper = require('services.server.world_generation.custom_landscaper')
-   local landscaper = radiant.mods.require('stonehearth.services.server.world_generation.landscaper')
-   radiant.mixin(landscaper, custom_landscaper)
+	local custom_landscaper = require('services.server.world_generation.custom_landscaper')
+	local landscaper = radiant.mods.require('stonehearth.services.server.world_generation.landscaper')
+	radiant.mixin(landscaper, custom_landscaper)
 
-   local custom_height_map_renderer = require('services.server.world_generation.custom_height_map_renderer')
-   local height_map_renderer = radiant.mods.require('stonehearth.services.server.world_generation.height_map_renderer')
-   radiant.mixin(height_map_renderer, custom_height_map_renderer)
+	local custom_height_map_renderer = require('services.server.world_generation.custom_height_map_renderer')
+	local height_map_renderer = radiant.mods.require('stonehearth.services.server.world_generation.height_map_renderer')
+	radiant.mixin(height_map_renderer, custom_height_map_renderer)
 
-   local custom_resource_call_handler = require('call_handlers.custom_resource_call_handler')
-   local resource_call_handler = radiant.mods.require('stonehearth.call_handlers.resource_call_handler')
-   radiant.mixin(resource_call_handler, custom_resource_call_handler)
+	local custom_resource_call_handler = require('call_handlers.custom_resource_call_handler')
+	local resource_call_handler = radiant.mods.require('stonehearth.call_handlers.resource_call_handler')
+	radiant.mixin(resource_call_handler, custom_resource_call_handler)
 end
 
 radiant.events.listen_once(radiant, 'radiant:required_loaded', archipelago_biome, archipelago_biome._on_required_loaded)

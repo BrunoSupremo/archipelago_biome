340fa1a6aef1c6afae22fa9ed1d9ca9c6b218a62
Bruno <bruno_mussoi@hotmail.com>
2018-08-24 20:46:35 -0300

    sand and grass in geomancer, bug fix

--- data/gm/campaigns/archipelago_unlock_stuff/arcs/trigger/encounters/unlock_stuff.lua
+++ data/gm/campaigns/archipelago_unlock_stuff/arcs/trigger/encounters/unlock_stuff.lua
@@ -1,10 +1,15 @@
 local Archipelago_Unlock_Stuff = class()
 
 function Archipelago_Unlock_Stuff:start(ctx, data)
-	local fisher_job = stonehearth.job:get_job_info(ctx.player_id, "archipelago_biome:jobs:fisher")
 	local biome = stonehearth.world_generation:get_biome_alias()
+	local fisher_job = stonehearth.job:get_job_info(ctx.player_id, "archipelago_biome:jobs:fisher")
 	if fisher_job and biome == "stonehearth:biome:arctic" then
 		fisher_job:manually_unlock_recipe("fishing:ice_dock")
+	end
+	local geomancer_job = stonehearth.job:get_job_info(ctx.player_id, "stonehearth:jobs:geomancer")
+	if geomancer_job then
+		geomancer_job:manually_unlock_recipe("terrain:archipelago_summon_stone_block_sand")
+		geomancer_job:manually_unlock_recipe("terrain:archipelago_summon_stone_patch_sand")
 	end
 end
 

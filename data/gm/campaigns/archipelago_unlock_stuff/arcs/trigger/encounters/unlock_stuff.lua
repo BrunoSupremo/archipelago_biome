c0d335c090698157ce616341a7ef7d7183fbf47b
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2023-02-27 23:40:03 -0300

    quick bug fix

--- data/gm/campaigns/archipelago_unlock_stuff/arcs/trigger/encounters/unlock_stuff.lua
+++ data/gm/campaigns/archipelago_unlock_stuff/arcs/trigger/encounters/unlock_stuff.lua
@@ -11,11 +11,6 @@ function Archipelago_Unlock_Stuff:start(ctx, data)
 	if fisher_job and biome == "stonehearth:biome:arctic" then
 		fisher_job:manually_unlock_recipe("fishing:ice_dock")
 	end
-	local geomancer_job = stonehearth.job:get_job_info(ctx.player_id, "stonehearth:jobs:geomancer")
-	if geomancer_job and biome == "archipelago_biome:biome:archipelago" then
-		geomancer_job:manually_unlock_recipe("terrain:archipelago_summon_stone_block_sand")
-		geomancer_job:manually_unlock_recipe("terrain:archipelago_summon_stone_patch_sand")
-	end
 end
 
 return Archipelago_Unlock_Stuff
